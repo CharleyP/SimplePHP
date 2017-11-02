24c26be4f5562f1adb24ac05381d36fa0c2201c8
pjc <214502706@qq.com>
2017-11-02 18:05:39 +0800

    正则替换输出缓存文件

--- lib/View.php
+++ lib/View.php
@@ -10,22 +10,46 @@ class View
 	function __construct()
 	{
 		if(empty($this->module)){
-			$module = APP::$module;
+			$this->module = APP::$module;
 		}
 		if(empty($this->action)){
-			$action = APP::$action;
+			$this->action = APP::$action;
+		}
+		if(empty($this->controller)){
+			$this->controller = APP::$controller;
 		}
 	}
 	public function getViewData(){
+
 		$view_path = getcwd()."/"."app/".$this->module."/View/".$this->action.".html";
 		$data = file_get_contents($view_path);
 		return $data;
 	}
 	public function assign($variable ,$value){
 		$view_data = $this->getViewData();
-		$test = "1234{{ $data }}9";
-		$view_data = preg_replace("/{{(\s+)*(\s+)}}/","qqqqq",$view_data);
-		echo $test1;
+		$view_data = preg_replace("/{{[\s\w\$]+}}/","<?php echo \$value; ?>",$view_data);
+		$cache_name = md5($this->module.$this->controller.$this->action).".php";
+		$root_path = getcwd()."/";
+		if(!file_exists($root_path."cache/".$this->module)){
+			mkdir($root_path."cache/".$this->module,0777);
+		}
+		//输出缓存文件
+		file_put_contents($root_path."cache/".$this->module."/".$cache_name, $view_data);
+
+
+		//$data = file_get_contents("");
+
+		$test = "<!DOCTYPE html>
+				<html>
+				<head>
+					<title></title>
+				</head>
+				<body>
+				<div><?php echo 'SELECT UID,EID,PHONE FROM p101' ?></div>
+				</body>
+				</html>";
+		// eval("\$test = \"$test\";");
+		echo $test;
 		exit;
 	}
 }
