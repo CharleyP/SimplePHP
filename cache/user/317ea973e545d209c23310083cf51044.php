199748e47274aa903966fd5370c9c64acc485cbe
CharleyP <214502705@qq.com>
2017-11-02 22:03:03 +0800

    缓存文件

---
+++ cache/user/317ea973e545d209c23310083cf51044.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div style="width:100px;height:100px;border:1px solid red"><?php echo $sql; ?></div>
+<div style="width:100px;height:100px;border:1px solid red"><?php echo $list; ?></div>
+</body>
+</html>
