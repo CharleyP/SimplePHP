a143af1aa60a5e726c8cc1dd374571516adb9198
pjc <214502706@qq.com>
2017-11-10 11:37:19 +0800

    修正mysqli基础类

--- lib/db/MysqliConn.php
+++ lib/db/MysqliConn.php
@@ -1,19 +1,30 @@
 <?php 
 namespace lib\db;
 
+use lib\App;
+
+
 class MysqliConn implements Mysql
 {
 	public $conn = null;
 	public $host = "localhost";
 	public $db = "test";
 	public $username = "root";
-	public $password = "root";
+	public $password = "";
 
 	public function __construct()
 	{
 		if($this->conn == null){
+			$this->getDatabaseInfo();
 			$this->connect();
 		}
+	}
+	public function getDatabaseInfo(){
+		$dataBaseInfo = APP::getConfig();
+		$this->host = $dataBaseInfo['hostname'];
+		$this->db = $dataBaseInfo['database'];
+		$this->username = $dataBaseInfo['username'];
+		$this->password = $dataBaseInfo['password'];
 	}
 	public function connect(){
 		$this->conn = new \Mysqli($this->host, $this->username, $this->password, $this->db);
@@ -25,12 +36,15 @@ class MysqliConn implements Mysql
 
 	}
 	public function select($query){
+		
 		$result = $this->conn->query($query);
 		$data=array();
 		while ($tmp=mysqli_fetch_assoc($result)) {
 		    $data[]=$tmp;
 		}
 		return $data;
+		// print_r($data);
+		// exit;
 	}
 	public function delete($query){
 
