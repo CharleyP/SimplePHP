fa56878afb40054aadd8d17aa6fe04e7a979d508
pjc <214502706@qq.com>
2017-11-14 17:14:43 +0800

    配置文件

--- config.php
+++ config.php
@@ -1,11 +1,27 @@
 <?php 
 
 return [
-
-	"ROOT_PATH"				=>	"/",
+	//默认配置可不动
 	"VIEW_FILE_TYPE"		=>	".html",
 	"TEMPLET_SIGN_LEFT"		=>	"{{",
 	"TEMPLET_SIGN_RIGHT"	=>	"}}",
+	
+	//项目目录
+	"ROOT_PATH"				=>	$_SERVER['DOCUMENT_ROOT']."/simplePHP/",
+	// MySQL类型
+    'conntype'            => 'mysqli',//请选择mysql/Mysql、mysqli/Mysqli、PDO(请勿使用小写)
+    // 服务器地址
+    'hostname'        => '127.0.0.1',
+    // 数据库名
+    'database'        => 'test',
+    // 用户名
+    'username'        => 'root',
+    // 密码
+    'password'        => 'root',
+    // 端口
+    'hostport'        => '3306',
+    // 数据库编码默认采用utf8
+    'charset'         => 'utf8',
 ]
 
 
